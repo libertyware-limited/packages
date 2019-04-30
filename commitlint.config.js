ae2cc398e0700eed4951bee30a2f60fd5220e11b
Jordan Hall <Jordan@libertyware.co.uk>
2019-04-30 20:05:16 +0100

    feat(init): add comment section

---
+++ commitlint.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'subject-max-length': [2, 'always', 120],
+    'scope-enum': [2, 'always', []],
+    'scope-empty': [2, 'always'],
+    'type-enum': [
+      2,
+      'always',
+      [
+        'build',
+        'ci',
+        'docs',
+        'feat',
+        'fix',
+        'perf',
+        'refactor',
+        'release',
+        'revert',
+        'style',
+        'test'
+      ]
+    ]
+  }
+};
